80e88c9d0ea8bdce91c6430de5a6f69b86377e73
Andrew Hua <33591358+andrewhua92@users.noreply.github.com>
2019-03-04 11:59:54 -0500

    fixed naming

--- ecedd_version_1__digitial_.ino
+++ ecedd_version_1__digitial_.ino
@@ -1,99 +1,103 @@
-int light1 = A0; // pin for the first photodiode
-int light2 = A1; // pin for the second photodiode
+int leftLight = A0; // pin for the first photodiode
+int rightLight = A1; // pin for the second photodiode
 // int light3 = 3;
 
-int lightState1 = 0;  // current state of the pin for the first photodiode
-int lightState2 = 0;
+int leftState = 0;  // current state of the pin for the first photodiode
+int rightState = 0;
 // int prevState1 = 0;
 // int prevState2 = 0;
 
-enum LocationStates{BOTH_ON, BOTH_OFF, NORTH_OFF, SOUTH_OFF};
+enum LocationStates{BOTH_ON, BOTH_OFF, LEFT_OFF, RIGHT_OFF};
 LocationStates currState = BOTH_ON;
 LocationStates prevState = currState;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(light1, INPUT);
-  pinMode(light2, INPUT);
+  pinMode(leftLight, INPUT);
+  pinMode(rightLight, INPUT);
   // pinMode(light3, INPUT);
 }
 
 
 
 void loop() {
-  lightState1 = analogRead(light1);
+  leftState = analogRead(leftLight);
 
-  lightState2 = analogRead(light2);
+  rightState = analogRead(rightLight);
 
   switch(currState){
     case BOTH_ON: {
       Serial.println("both on");
-      Serial.println(lightState1 + " and " + lightState2);
+      Serial.println(leftState);
+      Serial.println(rightState);
       Serial.println("");
-      if(lightState1 < 10 && lightState2 > 10){
-        currState = NORTH_OFF;
+      if(leftState < 10 && rightState > 10){
+        currState = LEFT_OFF;
         prevState = BOTH_ON;
       }
-      else if (lightState1 > 10 && lightState2 < 10){
-        currState = SOUTH_OFF;
+      else if (leftState > 10 && rightState < 10){
+        currState = RIGHT_OFF;
         prevState = BOTH_ON;
       }
       break;
     }
     case BOTH_OFF: {
       Serial.println("both off");
-      Serial.println(lightState1 + " and " + lightState2);
+      Serial.println(leftState);
+      Serial.println(rightState);      
       Serial.println("");
-      if (lightState1 < 10 && lightState2 > 10){
+      if (leftState < 10 && rightState > 10){
         // code to move
-        currState = NORTH_OFF;
+        currState = LEFT_OFF;
         prevState = BOTH_OFF;
       }
-      if (lightState1 > 10 && lightState2 < 10){
+      if (leftState > 10 && rightState < 10){
         // code to move
-        currState = SOUTH_OFF;
+        currState = RIGHT_OFF;
         prevState = BOTH_OFF;
       }
       break;
     }
-    case NORTH_OFF: {
-      Serial.println("north off");
-      Serial.println(lightState1 + " and " + lightState2);
+    case LEFT_OFF: {
+      Serial.println("left off");
+      Serial.println(leftState);
+      Serial.println(rightState);
       Serial.println("");
-       if (lightState1  < 10&& lightState2 > 10){
+       if (leftState  < 10&& rightState > 10){
         // code to move
        }
-       else if (lightState1 > 10 && lightState2 > 10){
+       else if (leftState > 10 && rightState > 10){
         currState = BOTH_ON;
-        prevState = NORTH_OFF;
+        prevState = LEFT_OFF;
        }
-       else if (lightState1 < 10 && lightState2 < 10){
+       else if (leftState < 10 && rightState < 10){
         currState = BOTH_OFF;
-        prevState = NORTH_OFF;
+        prevState = LEFT_OFF;
        }
        break;
     }
-    case SOUTH_OFF: {
-      Serial.println("south off");
-      Serial.println(lightState1 + " and " + lightState2);
+    case RIGHT_OFF: {
+      Serial.println("right off");
+      Serial.println(leftState);
+      Serial.println(rightState);
       Serial.println("");
-      if (lightState1 > 10 && lightState2 < 10){
+      if (leftState > 10 && rightState < 10){
           // code to move
       }
-      else if (lightState1 > 10 && lightState2 > 10){
+      else if (leftState > 10 && rightState > 10){
         currState = BOTH_ON;
-        prevState = SOUTH_OFF;
+        prevState = RIGHT_OFF;
       }
-      else if (lightState1 < 10 && lightState2 < 10){
+      else if (leftState < 10 && rightState < 10){
         currState = BOTH_OFF;
-        prevState = SOUTH_OFF;
+        prevState = RIGHT_OFF;
       }
       break;
     }
   }
 
-  // prevState1 = lightState1;
-  // prevState2 = lightState2; 
+  // prevState1 = leftState;
+  // prevState2 = rightState; 
 
-  delay(1000);
+  delay(100);
 }
